7b2a32f77530a37ecbb483197e9a145ebf95ff9b
Albinosos <laklaym698@gmail.com>
2024-12-06 14:28:19 +0200

    Реалізація класу StandardCalculator

--- calculator/calculator/StandardCalculator.cs
+++ calculator/calculator/StandardCalculator.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace calculator
+namespace ConsoleApp1
 {
-    internal class StandardCalculator
+    public class StandardCalculator
     {
+        protected double Operand1 { get; set; }
+        protected double Operand2 { get; set; }
+
+        public StandardCalculator()
+        {
+            Operand1 = 0;
+            Operand2 = 0;
+        }
+
+        public double Add() => Operand1 + Operand2;
+        public double Subtract() => Operand1 - Operand2;
+        public double Multiply() => Operand1 * Operand2;
+        public double Divide()
+        {
+            if (Operand2 == 0)
+                throw new DivideByZeroException("Ділення на нуль!");
+            return Operand1 / Operand2;
+        }
+
+        public void SetOperands(double operand1, double operand2)
+        {
+            Operand1 = operand1;
+            Operand2 = operand2;
+        }
     }
 }
