d944558772e625a7455e55977460d1497e209bde
Albinosos <laklaym698@gmail.com>
2024-12-06 14:30:19 +0200

    Реалізація класу EngineeringCalculator

--- calculator/calculator/EngineeringCalculator.cs
+++ calculator/calculator/EngineeringCalculator.cs
@@ -4,9 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace calculator
+namespace ConsoleApp1
 {
-    internal class EngineeringCalculator
+    public class EngineeringCalculator : StandardCalculator
     {
+        public EngineeringCalculator() : base() { }
+
+        public double Exp()
+        {
+            if (Operand1 < -5 || Operand1 > 5)
+                throw new ArgumentOutOfRangeException("Аргумент операції exp повинен бути в межах від -5 до 5!");
+            return Math.Exp(Operand1);
+        }
+
+        public double SinInDegrees()
+        {
+            // Перетворення градусів у радіани
+            double radians = DegreesToRadians(Operand1);
+            return Math.Sin(radians);
+        }
+
+        public double CosInDegrees()
+        {
+            // Перетворення градусів у радіани
+            double radians = DegreesToRadians(Operand1);
+            return Math.Cos(radians);
+        }
+
+        private double DegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180;
+        }
+
+        private double RadiansToDegrees(double radians)
+        {
+            return radians * 180 / Math.PI;
+        }
     }
 }
